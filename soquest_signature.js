4509cc25aeecb50a138046c46f0af7d59d31f25c
0xygen77 <125109141+0xygen77@users.noreply.github.com>
2023-11-25 16:13:45 +0800

    Update soquest_signature.js

--- soquest_signature.js
+++ soquest_signature.js
@@ -1,7 +1,7 @@
 let showNotification = true;
 
 function surgeNotify(subtitle = '', message = '') {
-  $notification.post('SoQuest signature', subtitle, message, { 'url': '' });
+  $notification.post('SoQuest 必要資訊', subtitle, message, { 'url': '' });
 };
 
 function handleError(error) {
@@ -23,16 +23,28 @@ async function getToken() {
     try {
       const signature = $request.headers['signature'] || $request.headers['Signature'];
       const address = $request.headers['address'] || $request.headers['Address'];
-      if (signature && address) {
+      const apikey = $request.headers['Apikey'] || $request.headers['apikey'];
+      const time = $request.headers['Time'] || $request.headers['time'];
+      const sign = $request.headers['Sign'] || $request.headers['sign'];
+      
+      if (signature && address && apikey && time && sign) {
         const sig_save = $persistentStore.write(signature, 'SoQuestSignature');
         const adr_save = $persistentStore.write(address, 'SoQuestAddress');
-        if (sig_save && adr_save) {
+        const sign_save = $persistentStore.write(sign, 'SoQuestSign');
+        const time_save = $persistentStore.write(time, 'SoQuestTime');
+        const api_save = $persistentStore.write(apikey, 'SoQuestApikey');
+        if (sig_save && adr_save && sign_save && time_save && api_save) {
           return resolve();
         } else {
-          return reject(['保存失敗 ‼️', '無法儲存 signature 或是 address']);
+          console.log('Signature: '+ sig_save);
+          console.log('\nAddress: '+ adr_save);
+          console.log('\nSign: '+ sign_save);
+          console.log('\nTime: '+ time_save);
+          console.log('\nApi: '+ api_save);
+          return reject(['保存失敗 ‼️', '儲存 signature, address, sign, time, apikey 發生錯誤']);
         }
       } else {
-        return reject(['保存失敗 ‼️', '無法取得 signature 或是 address']);
+        return reject(['保存失敗 ‼️', '無法取得 signature, address, sign, time, apikey']);
       }
     } catch (error) {
       return reject(['保存失敗 ‼️', error]);
@@ -41,10 +53,10 @@ async function getToken() {
 }
 
 (async () => {
-  console.log('ℹ️ SoQuest 取得 signature v20230411.1');
+  console.log('ℹ️ SoQuest 取得 signature v20231125.1');
   try {
     await getToken();
-    console.log('✅ signature 保存成功');
+    console.log('✅ SoQuest 必要資訊保存成功');
     surgeNotify(
       '保存成功 🍪',
       ''
